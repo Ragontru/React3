0cde35e5c621a577f38f03b296497f825609a06a
Ragontru <ragontru25@gmail.com>
2021-05-14 11:13:03 +0200

    Update EjercicioComponent.js

--- ejercicio4/src/components/EjercicioComponent.js
+++ ejercicio4/src/components/EjercicioComponent.js
@@ -4,59 +4,51 @@ class EjercicioComponent extends Component {
 
     constructor(props) {
         super(props);
-        carrito = this.state;
+        this.state = {
+            carritotal: 0,
+            carrito: ''
+        };
         this.alCarrito = this.alCarrito.bind(this);
     }
 
     render() {
         return (
-            <div class="container">
-                <div class="items">
-                    <form onSubmit={this.alCarrito}>
-                        <h2 class="text-center">TIENDA</h2>
-                        <p>
-                            <label>Producto: </label>
-                            <select>
-                                <option id="pan" value="pan">Pan</option>
-                                <option id="leche" value="leche">Leche</option>
-                                <option id="nueces" value="nueces">Nueces</option>
-                                <option id="manzana" value="manzana">Manzana</option>
-                            </select>
+            <div className="App">
 
-                        </p>
-                        <p>
-                            <label>Precio: </label>
-                            <input id="precio" type="number" />
-                        </p>
-                        <p>
-                            <label>Cantidad: </label>
-                            <input id="cant" type="number" />
-                        </p>
-                        <p>
-                            <button class="item-button" onClick="alCarrito">Añadir a la lista</button>
-                        </p>
-                    </form>
-                </div>
+                <form onSubmit={this.alCarrito}>
+                    <h2 className="text-center">TIENDA</h2>
+                    <p>
+                        <label>Producto: </label>
+                        <select name="producto">
+                            <option id="pan" value="pan">Pan</option>
+                            <option id="leche" value="leche">Leche</option>
+                            <option id="nueces" value="nueces">Nueces</option>
+                            <option id="manzana" value="manzana">Manzana</option>
+                        </select>
+
+                    </p>
+                    <p>
+                        <label>Precio: </label>
+                        <input id="precio" type="number" />
+                    </p>
+                    <p>
+                        <label>Cantidad: </label>
+                        <input id="cantidad" type="number" />
+                    </p>
+                    <p>
+                        <input type="submit" value="Añadir a la lista" onSubmit={this.alCarrito}></input>
+                    </p>
+                </form>
+
                 <hr></hr>
-                <div class="row">
-                    <h2 class="text-center">Lista de la compra</h2>
-                    <div class="shopping-cart-header">
-                        <h6 class="text-truncate">Producto</h6>
-                    </div>
-                    <div class="shopping-cart-header">
-                        <h6 class="text-truncate">Precio</h6>
-                    </div>
-                    <div class="shopping-cart-header">
-                        <h6 class="text-truncate">Cantidad</h6>
-                    </div>
+                <div className="row">
+                    <h2 className="text-center">Lista de la compra</h2>
+                    <h6 className="text-truncate">Producto</h6>
                 </div>
 
-                <div class="shopping-cart-items shoppingCartItemsContainer"></div>
-
-                <div class="row">
-                    <div class="shopping-cart-total align-items-center">
-                        <p class="mb-0">Total</p>
-                        <p class="shoppingCartTotal">0€</p>
+                <div className="row">
+                    <div>
+                        <p className="mb-0">Total</p>
                     </div>
                 </div>
 
@@ -68,14 +60,20 @@ class EjercicioComponent extends Component {
     alCarrito(event) {
         event.preventDefault();
 
-        this.setState((state) => {
-            return {carrito: this.state.carrito + nuevaLinea}
-        });
+        var prod = (event.target.producto.value);
+        var cant = parseInt(event.target.cantidad.value);
+        var prc = parseInt(event.target.precio.value);
+        var totalArt = cant * prc;
+        var factura = cant + ' ' + prod + ' : ' + totalArt + ' €. \n';
+        // actualizar aparte
+        var total = totalArt;
 
-        const v1 = event.target.precio.value;
-        const v2 = event.target.cant.value;
-        const suma = suma + v1 * v2;
-        alert('El carrito  del ' + suma + ' euros.');
+        console.log(factura);
+        console.log(total);
+
+        this.setState((state) => {
+            return { carrito: factura, carritotal: total }
+        });
     }
 
 }
